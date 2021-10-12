c4724554719aa2ead3292494b3c5dc02d4f860bc
12-RachelAng <cherryzx04@gmail.com>
2021-10-12 17:31:02 +0800

    Collision for the Coin with Player
    Collides with Coin will Score 10 Points

---
+++ Assets/Script/CollisionScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionScript : MonoBehaviour
+{
+    //Scoring
+    public Text ScoreText;
+    public int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = "Coin: " + Score;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            Score += 10;
+            ScoreText.text = "Coin: " + Score;
+
+            Destroy(other.gameObject);
+        }
+    }
+}
